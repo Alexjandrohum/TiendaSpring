1c32ff1962d6746f06b196069c91fcf2e63e627f
Alexjandrohum <alexjandrohum@Alexjandrohums-MacBook-Pro.local>
2020-10-27 23:27:17 -0600

    Se agregó la funcionalidad e agregar productos

--- src/main/java/com/spring/web/ProductoController.java
+++ src/main/java/com/spring/web/ProductoController.java
@@ -7,12 +7,14 @@ package com.spring.web;
 
 import com.spring.domain.Producto;
 import com.spring.servicio.ProductoService;
+import java.util.Date;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  *
@@ -31,6 +33,15 @@ public class ProductoController {
         model.addAttribute("productos", listaProductos);
       
         return "listadoProductos";
+    }
+    
+    @PostMapping("/guardarProducto")
+    public String guardarProducto(Producto productos){
+        productos.setFechaCreado(new Date());
+        productos.setFechaEditado(new Date());
+        productos.setEstado("activo");
+        productoService.agregarProducto(productos);
+        return "redirect:/listarProductos";
     }
     
 }
